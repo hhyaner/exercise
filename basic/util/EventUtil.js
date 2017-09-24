a60826f360b8c6a9cf2b16f42e3afefab5cef205
Victoria <hhyaner@gmail.com>
2017-09-24 16:05:37 +0800

    form text

--- basic/util/EventUtil.js
+++ basic/util/EventUtil.js
@@ -92,5 +92,17 @@ var EventUtil = {
         else {
             event.cancelBubble = true;
         }
+    },
+    getClipboardText: function(event) {
+        var clipboardData = (event.clipboardData || window.clipboardData);
+        return clipboardData.getData('text');
+    },
+    setClipboardText: function(event, value) {
+        if (event.clipboardData) {
+            return event.clipboardData.setData('text/plain', value);
+        }
+        else if (window.clipboardData) {
+            return window.clipboardData.setData('text', value);
+        }
     }
 }
