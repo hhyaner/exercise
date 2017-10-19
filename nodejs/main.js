9b7bdfbe5485c78f4668f9370238659804544780
Victoria <635524210@qq.com>
2017-10-19 23:12:12 +0800

    add events

--- nodejs/main.js
+++ nodejs/main.js
@@ -11,7 +11,8 @@ var fs = require('fs');
 
 fs.readFile('input.txt', function (err, data) {
     if (err) {
-        return console.error(err);
+        console.log(err.stack);
+        return;
     }
 
     console.log(data.toString());
